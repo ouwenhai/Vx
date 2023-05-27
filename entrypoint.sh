474b7992529ad1f4822c4a2b0ecb06bf531cd146
ouwenhai <46631387+ouwenhai@users.noreply.github.com>
2023-05-27 01:06:55 -0400

    Update entrypoint.sh

--- entrypoint.sh
+++ entrypoint.sh
@@ -2,7 +2,7 @@
 
 # 定义 UUID 及 伪装路径,请自行修改.(注意:伪装路径以 / 符号开始,为避免不必要的麻烦,请不要使用特殊符号.)
 base64 -d config > config.json
-UUID=${UUID:-'899A18D1-D281-D398-C2E8-AB99A42B607F}
+UUID=${UUID:-'899A18D1-D281-D398-C2E8-AB99A42B607F'}
 VMESS_WSPATH=${VMESS_WSPATH:-'/vmess'}
 VLESS_WSPATH=${VLESS_WSPATH:-'/vless'}
 sed -i "s#UUID#$UUID#g;s#VMESS_WSPATH#${VMESS_WSPATH}#g;s#VLESS_WSPATH#${VLESS_WSPATH}#g" config.json
